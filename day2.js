863889f36f442f447ace4be3bd33878bb57e8676
asthabasnet <aastha.basnets24@cps.edu.np>
2025-12-07 07:45:11 +0545

    feat:js basics

---
+++ day2.js
@@ -0,0 +1,27 @@
+const collegeName =["astha","pari","pariz"];
+const name ={
+    id: 1,
+    firstName:"astha",
+    colleges: collegeName,
+};
+console.log(collegeName[2]);
+console.log(name.colleges[2]);
+
+const students =["astha","pasta","nasta","rasta"];
+console.log(students.length);
+ const collegeDetails={
+    name:"techspire",
+    section:20,
+    students:students,
+    award:null,
+ };
+ console.log(collegeDetails.students[3]);
+
+ function getStudentName(){
+    console.log("i am normal func");
+ }
+ getStudentName();
+
+const getColleName = () => {
+    console.log("am arrow func");
+};
